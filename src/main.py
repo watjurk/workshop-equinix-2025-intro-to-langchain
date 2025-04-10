5ac9b0286c0d9540f7015738f54d8d4ac8691573
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-04-10 13:36:56 +0200

    add groq support

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import getpass
+import os
+
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+if not os.environ.get("GROQ_API_KEY"):
+    os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("llama-3.3-70b-versatile", model_provider="groq")
+
+
+print(model.invoke("mleko"))
